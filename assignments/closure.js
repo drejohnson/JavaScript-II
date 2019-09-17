9ccbaccd40446a79e73de62ba0790558cfa98561
DeAndre Johnson <dre@phreshr.com>
2019-09-18 04:29:55 +0800

    add closure example

--- assignments/closure.js
+++ assignments/closure.js
@@ -3,6 +3,17 @@
 // Keep it simple! Remember a closure is just a function
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
+function sayHello(name) {
+  const greeting = "Hello";
+  const hello = () => {
+    return `${greeting} ${name}`
+  }
+  return hello;
+}
+
+let greet = sayHello('Dre')
+
+console.log(greet())
 
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
