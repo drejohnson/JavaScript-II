942d013bfd12bcf60f2df9edfde544e36ba3b229
DeAndre Johnson <dre@phreshr.com>
2019-09-18 03:27:54 +0800

    complete callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -41,24 +41,63 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length)
 }
 
+function logLength(items) {
+  console.log(items)
+}
+
+getLength(items, logLength)
+
+// 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr)
 }
 
+function logLast() {
+  console.log(items.slice(-1));
+}
+
+last(items, logLast)
+
+// 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x,y)
 }
 
+function logAdd(x, y) {
+  console.log(x + y);
+} // callback
+
+sumNums(3,4, logAdd)
+
+// 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x,y)
 }
 
+function logMultiply(x, y) {
+  console.log(x * y);
+} // callback
+
+multiplyNums(6, 6, logMultiply)
+
+// 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(item, list)
 }
+
+function logContains(item, list) {
+  console.log(list.includes(item))
+}
+
+contains("Pencil", items, logContains)
 
 /* STRETCH PROBLEM */
 
